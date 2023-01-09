dd41ee3c174c582dfc1db2ebf5f4418edf9adf51
Orion_IX <76408216+Clement-XVII@users.noreply.github.com>
2023-01-09 19:39:36 +0100

    Update StatusMonitor.sh

--- StatusMonitor/StatusMonitor.sh
+++ StatusMonitor/StatusMonitor.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+
+
 # Function to read the list of hosts from the file
 read_hosts() {
-  HOSTS=()
-  NAMES=()
-  while read -r line; do
-    HOSTS+=("$(echo "$line" | cut -d ',' -f1)")
-    NAMES+=("$(echo "$line" | cut -d ',' -f2)")
-  done < "$HOST_FILE"
+    HOST_FILES=$1
+    IFS=',' read -ra HOST_FILES <<< "$HOST_FILES"
+  for HOST_FILE in "${HOST_FILES[@]}"; do
+    while read -r line; do
+      HOSTS+=("$(echo "$line" | cut -d ',' -f1)")
+      NAMES+=("$(echo "$line" | cut -d ',' -f2)")
+    done < "$HOST_FILE"
+  done
 }
+
 
 print_table() {
    printf "%-20s %-20s %-20s %-10s %s\n" "IP Address" "DNS" "Name" "Latency" "Status"
@@ -18,15 +23,13 @@ print_table() {
 # Function to ping a host and return the result
 ping_host() {
   HOST=$1
-  PING_RESULT=$(ping -q -c 1 -w 1 "$HOST")
+  PING_RESULT=$(ping -q -c 1 -w 1 "$HOST") &> /dev/null
   if [ $? -eq 0 ]; then
-    DNS=$(nslookup "$HOST" | grep "name = " | awk '{print $NF}')
+    DNS=$(nslookup "$HOST" -timeout=1 | grep "name = " | awk '{print $NF}') &> /dev/null
     LATENCY=$(echo "$PING_RESULT" | tail -1 | awk '{print $4}' | cut -d '/' -f 2) &> /dev/null
     printf "%-20s %-20s %-20s %-10s %s\n" "$HOST" "$DNS" "$NAME" "$LATENCY ms" "[$(tput setaf 2) UP $(tput sgr0)]"
   else
-    DNS=$(nslookup "$HOST" | grep "name = " | awk '{print $NF}')
-    printf "%-20s %-20s %-20s %-10s %s\n" "$HOST" "$DNS" "$NAME" "-" "[$(tput setaf 1)DOWN$(tput sgr0)]"
-    DOWN_COUNT=$((DOWN_COUNT + 1))
+    printf "%-20s %-20s %-20s %-10s %s\n" "$HOST" "-" "$NAME" "-" "[$(tput setaf 1)DOWN$(tput sgr0)]"
   fi
 }
 
@@ -36,21 +39,24 @@ print_status() {
   for i in "${!HOSTS[@]}"; do
     HOST=${HOSTS[$i]}
     NAME=${NAMES[$i]}
-    ping_host "$HOST"
+    ping_host "$HOST" &
   done
+  wait
 }
 
-# Function to print the summary of the host status
 print_summary() {
+  # Get the current time
+  current_time=$(date +'%H:%M:%S')
+
   echo ""
+  DOWN_COUNT=$(grep -c "DOWN" <<< "$(print_status)")
   if [ "$DOWN_COUNT" == 0 ]; then
     echo "Summary @ $current_time: All hosts are $(tput setaf 2)UP$(tput sgr0)"
   else
-    echo "Summary @ $current_time: $DOWN_COUNT out of ${#HOSTS[@]} hosts are $(tput setaf 1)DOWN$(tput sgr0)"
+    echo "Summary @ $current_time: $(tput setaf 1)$DOWN_COUNT$(tput sgr0) out of ${#HOSTS[@]} hosts are $(tput setaf 1)DOWN$(tput sgr0)"
   fi
   SECONDS_REMAINING=$INTERVAL
- 
-until [ "$SECONDS_REMAINING" == 0 ]; do
+  until [ "$SECONDS_REMAINING" == 0 ]; do
     printf "\rRefreshing in %02d seconds... " "$SECONDS_REMAINING"
     sleep 1
     SECONDS_REMAINING=$((SECONDS_REMAINING - 1))
@@ -59,13 +65,12 @@ until [ "$SECONDS_REMAINING" == 0 ]; do
 
 # Main script
 INTERVAL=15 # Default interval is 15 minutes
-HOST_FILE=hosts.lst # Default host file is hosts.lst
 
 # Parse options
 while getopts ":f:" opt; do
   case $opt in
     f)
-      HOST_FILE=$OPTARG
+      read_hosts "$OPTARG"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -78,7 +83,7 @@ while getopts ":f:" opt; do
   esac
 done
 
-# Read host list from specified file
+# Read host list from specified files or default file
 read_hosts
 
 # Continuously ping hosts at the specified interval
